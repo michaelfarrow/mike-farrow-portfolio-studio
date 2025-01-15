b9d891f841d4641d6de7214ae228f86d44f0f2ba
Mike Farrow <mike@farrow.io>
2025-01-15 12:22:02 +0000

    fixing error typos

--- lib/env.ts
+++ lib/env.ts
@@ -4,9 +4,10 @@ export const STUDIO_DATASET = process.env.SANITY_STUDIO_DATASET;
 export const STUDIO_API_VERSION = process.env.SANITY_STUDIO_API_VERSION;
 export const APP_BASE_URL = process.env.SANITY_STUDIO_APP_BASE_URL;
 
-if (!STUDIO_PROJECT_ID) throw new Error('STUDIO_PROJECT_ID not defined');
-if (!STUDIO_DATASET) throw new Error('STUDIO_DATASET not defined');
-if (!STUDIO_API_VERSION) throw new Error('STUDIO_API_VERSION not defined');
+if (!STUDIO_PROJECT_ID) throw new Error('SANITY_STUDIO_PROJECT_ID not defined');
+if (!STUDIO_DATASET) throw new Error('SANITY_STUDIO_DATASET not defined');
+if (!STUDIO_API_VERSION)
+  throw new Error('SANITY_STUDIO_API_VERSION not defined');
 
 export const STUDIO_CONFIG = {
   projectId: STUDIO_PROJECT_ID,
