8a29db2ad6ec3930c62897ecb4b0d48242f465ce
Mike Farrow <mike@farrow.io>
2025-02-10 22:02:37 +0000

    type fix

--- schemas/index.ts
+++ schemas/index.ts
@@ -5,14 +5,7 @@ import { contact } from '@/schemas/contact';
 import { link } from '@/schemas/link';
 import { project } from '@/schemas/project';
 
-export const schemas = [
-  common,
-  category,
-  contact,
-  project,
-  link,
-  album,
-] as const;
+export const schemas = [common, category, contact, project, link, album];
 
 export type Schemas = (typeof schemas)[number];
 export type SchemaType = Schemas['name'];
